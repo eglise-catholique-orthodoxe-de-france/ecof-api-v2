18f4b8e4c017a5ab862b666f5283ff926aa2a1c5
JacquesRaoult <raoult.jacques@gmail.com>
2026-08-26 18:12:47 +0200

    Remove static files

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import { Hono } from "hono";
-import { serveStatic } from "hono/bun";
 import { synaxarRoutes } from "./features/synaxar/synaxar.routes";
 import { mapRoutes } from "./features/map/map.routes";
 
 const app = new Hono();
 
-// Static pages
-app.use("/*", serveStatic({ root: "./src/public" }));
-
-// Features
 app.route("/synaxar", synaxarRoutes);
 app.route("/map", mapRoutes);
 
