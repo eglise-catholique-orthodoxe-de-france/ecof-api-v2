fbc071ba529b937ef964588eeed8b31450cf6b43
JacquesRaoult <raoult.jacques@gmail.com>
2026-08-26 17:48:50 +0200

    Add calendar functions

---
+++ src/features/liturgical-calendar/liturgical-calendar.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "bun:test";
+import { getEasterDate } from "./liturgical-calendar.utils";
+
+describe("getEasterDate", () => {
+  test("returns Easter Sunday for 2026", () => {
+    expect(getEasterDate(2026)).toEqual(Temporal.PlainDate.from("2026-04-05"));
+  });
+});
